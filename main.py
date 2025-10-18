b8490c97dfa3a7478e47bb52a5a22c8a6f14654b
Haschwallth <rip56709@protonmail.com>
2025-10-17 22:04:29 -0300

    Tarefas

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #importação de bibliotecas
 from flask import Flask
+from tarefa import buscar_tarefas
 
 #cria o objeto deo flask
 app = Flask(__name__)
+
+#criando a rota que retorna as tarefas
+@app.route('/api/tarefas')
+def get_tarefa():
+    tarefas = buscar_tarefas()
+    return tarefas
 
 #criando nossa primeira rota /api
 @app.route('/api')
